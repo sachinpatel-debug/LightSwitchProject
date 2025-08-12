dc0e8dfb3f8ca2858697337ff865aa72d01278d7
Sachin Patel <sachinp9292@gmail.com>
2025-08-12 10:45:36 -0400

    Update InternetReceiveSketch.ino

--- InternetReceiveSketch.ino
+++ InternetReceiveSketch.ino
@@ -1,47 +1,58 @@
 #include <ServoTimer2.h>
 
 #include <RH_ASK.h>
-#include <SPI.h> // Not actualy used but needed to compile
+#include <SPI.h> // Not actually used but needed to compile
 #include <string.h>
+#include <avr/interrupt.h>
+#define wdt_reset() __asm__ __volatile__("wdr");
+const int LED = 13;
+volatile bool wakeUpTime = false;
 
-// #include <Servo.h>
-// Servo myServo;
 ServoTimer2 myServo;
 RH_ASK driver;
 int lightStatus = 0;
-int photoResistance = 0;
+uint8_t buf[2];
+uint8_t buflen = sizeof(buf);  
+int timeElapsed;
+ISR(WDT_vect){
+wakeUpTime = true;
+}
 void setup()
 {
-  // Serial.begin(9600);	
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  Serial.begin(9600);	
   driver.init();
   // Serial.println("This is the receiver sketch"); 
-  myServo.attach(3);
+  // myServo.attach(3);
   // pinMode(7, OUTPUT);
-  myServo.write(1675);
+  // myServo.write(1675);
+  cli();
+  wdt_reset();
+  WDTCSR |= (1<<WDCE) | (1<<WDE);
+  WDTCSR = (1 << WDIE) | (1<<WDP2) | (1<<WDP1); 
+  sei();
 
-  
 }
 
 void loop()
 {
-  photoResistance = analogRead(A0);
-  Serial.println(photoResistance);
-  // randomSeed(analogRead(A0));
-  // int randomNumber=random(180);
-  // int buttonStatus = digitalRead(11);
-  // if(buttonStatus==1){
-  //   Serial.print(buttonStatus);
-  //   myServo.write(randomNumber);
-  //   delay(10000);
-  // }
-  // int pinStatus = digitalRead(7);
-  // if(pinStatus==1){
-  //   Serial.print(pinStatus);
-  // }
-  
-    uint8_t buf[2];
-    uint8_t buflen = sizeof(buf);
+  if(wakeUpTime == false){
+    //sleep code here
+    ADCSRA &= ~(1<<7);
+    SMCR |= (1<<2);
+    SMCR |= 1; 
+
+    MCUCR |= (3<<5);
+    MCUCR |= (MCUCR & ~(1 << 5)) | (1 << 6);
+
+    __asm__ __volatile__("sleep");
     
+  }
+
+  else if(wakeUpTime ==true){
+unsigned long timeBeforeLoop = millis();
+    do{
     if (driver.recv(buf, &buflen)) // Non-blocking
     {
     
@@ -49,61 +60,48 @@ void loop()
       // Serial.print("Message: ");
       // Serial.println((char*)buf);         
     }
-
-    // Serial.print("This is buf 0: ");
-    // Serial.println(buf[0]);
-    // Serial.print("This is buf 1: ");
-    // Serial.println(buf[1]);
-    // Serial.print("This is buf 2: ");
-    // Serial.println(buf[2]);
-    // Serial.print("This is buf 3: ");
-    // Serial.println(buf[3]);
-
-    // Serial.print("This is buf 1 characters: ");
     
-    // // Serial.print(char(buf[1]));
-    // Serial.println((char*)buf[1]);
-    // Serial.print("This is buf 2 characters: ");
-    // Serial.print(char(buf[2]));
-    // Serial.println((char*)buf[2]);
-    // Serial.print("This is buf 3 characters: ");
-    // Serial.print(char(buf[3]));
-    // Serial.println((char*)buf[3]);
-    // Serial.print("This is the full message  ");
-    // Serial.print((char*)buf);
-    // Serial.println("      ");
-    // Serial.println((char)buf);         
-    String message = (char*)buf;
+  String message = (char*)buf;
   message = message.substring(0, 2);    
-  Serial.print("This is the message as a string: ");
-    Serial.println(message);
+  //  Serial.println(message);
     if(message == "Ga"){
-      // digitalWrite(7, HIGH);
       buf[0] = 0;
       buf[1] = 0;
-      // delay(500);
-      // digitalWrite(7, LOW);
       
-      if(photoResistance > 705){
-        myServo.write(2250);
-        delay(1000);
-        myServo.write(1685);
-        // digitalWrite(7, LOW);
-        // lightStatus = 0;
-        // Serial.print("The light status is ");
-        // Serial.println(lightStatus);
+      if(lightStatus == 1){
+        // myServo.write(2250);
+        // delay(1000);
+        // myServo.write(1685);
+        digitalWrite(13, HIGH);
+        delay(700);
+        digitalWrite(13, LOW);
+        lightStatus = 0;
+      
       }
-      if(photoResistance < 705){
-        myServo.write(1400);
-        delay(1000); 
-        myServo.write(1675);        // digitalWrite(7,HIGH);
-        // lightStatus = 1;
-        // Serial.print("The light status is ");
-        // Serial.println(lightStatus);
+      else if(lightStatus == 0){
+        // myServo.write(1400);
+        // delay(1000); 
+        // myServo.write(1675);       
+          digitalWrite(13, HIGH);
+          delay(700);
+          digitalWrite(13, LOW);
+          lightStatus = 1;
       }
-    }
+      wakeUpTime = false;
+      delay(250);
 
-    delay(500);
+    }  
+  unsigned long timeAfterLoop = millis();
+  timeElapsed = timeAfterLoop - timeBeforeLoop;
+  Serial.println(timeElapsed);
+  }
+  while((timeElapsed <= 300) && (wakeUpTime == true));
+  wdt_reset();
+  wakeUpTime = false;
+  }
+  
+
+
     
     
   
